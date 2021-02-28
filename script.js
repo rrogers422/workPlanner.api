e7c5ca2d4d46940f68604afba44dec800dc5f133
Reagan Rogers <reagan422440@gmail.com>
2021-02-28 16:54:15 -0500

    Made changes to JS syntax to save textinfo into local storage

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ var currentDate = $('#currentDay');
 //display for current date//
 currentDate.text(moment().format('MMM Do, YYYY'));
 
-
 //Function that calls past,present, future 
 var hourContainers = $(".row");
 hourContainers.each(function(index) {
@@ -25,17 +24,18 @@ hourContainers.each(function(index) {
 //Variable for local storage
 var saveSchedule = window.localStorage;
 var saveInfo = [];
-var i = 0;
+//var i = 0;
+let textInfo
 
 /*.on click function to save info thats input to local strage 
 sets variables to grab value from the text area and the attribute on ID
-pushes into a string*/
+pushes into a string in local*/
 
 $('.saveBtn').on('click', function () {
     let textInfo = $(this).siblings('textarea').val();
     let rowId = $(this).attr('id');
     saveInfo.push({ rowId, textInfo });
-    saveSchedule.setItem('savedItems', JSON.stringify(saveInfo))
+    saveSchedule.setItem('textInfo', JSON.stringify(saveInfo))
 });
 
 
@@ -43,10 +43,9 @@ var displayInfo = [];
 if (saveSchedule.length > i) {
     displayInfo = JSON.parse(saveSchedule.getItem('saveInfo'));
     for (var i = 0; i < displayInfo.length; i++) {
-        $('$(.row)').children('textarea').val(displayInfo[i].textInput)
+        $('$.row)').children('textarea').val(displayInfo[i].textInput)
     }
 };
-
 
 
 //Comment lines are rough drafts of local storage and onclick funtion/inputs
